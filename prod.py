bfe38a90a50b463d2ee01d020883610eabd19b98
Cameron Carmichael Alonso <cameron@carmichaelalonso.co.uk>
2015-08-18 00:01:30 +0200

    Update prod.py

--- prod.py
+++ prod.py
@@ -2,4 +2,19 @@
 ## Converts json to dat for use in IF
 ##
 
-print "This is printed from Python"
+import datetime
+import sys
+
+## create array for lines
+lines = []
+
+## add headers
+lines.append("I")
+lines.append("810 Version - Created on " + datetime.datetime.now())
+
+## save to file
+with open(sys.argv[1]) as lines, open('out.txt', 'w') as out:
+    for line in lines:
+       if line.strip():                     #checks if line is not empty
+           out.write(line.split()[0]+'\n')
+           print line
